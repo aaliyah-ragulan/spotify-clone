9acff27f765bed62855a6c4939257f407eb4ce21
ragulan <ragulanravi21@gmail.com>
2021-05-19 11:14:08 -0400

    added loginjs

---
+++ src/Login.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const AUTH_URL =
+  'https://accounts.spotify.com/authorize?client_id=&response_type=code&redirect_uri=http://localhost:3000&scope=%20'
+
+export default function Login() {
+  return <div></div>
+}
